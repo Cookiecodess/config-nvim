349c03a3e90844ded95bbecb892bd4df2ad08286
Cookie <jasonlhtown@gmail.com>
2026-01-16 22:27:34 +0800

    add autocmd: autosave

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- autosave on normal mode
+vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave", "FocusLost", "BufLeave" }, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.modifiable and vim.bo.filetype ~= "help" then
+      vim.cmd("silent! update") -- saves if the buffer is modified
+    end
+  end,
+})
