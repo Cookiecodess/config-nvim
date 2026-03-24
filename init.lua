ff9096540de41ae8dfbc9f9bd1dbf9208d3f6bcc
Cookie <jasonlhtown@gmail.com>
2026-03-24 23:23:33 +0800

    disable pycodestyle

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local lspconfig = require("lspconfig")
+
+lspconfig.pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = { enabled = false },
+        -- optionally disable other style linters if you want:
+        -- pyflakes = { enabled = false },
+        -- mccabe = { enabled = false },
+      },
+    },
+  },
+}
+
