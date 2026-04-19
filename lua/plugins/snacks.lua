fd2683045ce1228f73da1d1c90ef60624989a5e1
Cookie <jasonlhtown@gmail.com>
2026-04-19 17:32:13 +0800

    attempted to make file explorer not change directory shown when i open a file deep within current dir, didnt seem to work

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,24 @@ return {
   {
     "snacks.nvim",
     opts = {
+      explorer = {
+        follow_file = false,
+      },
+      -- -- Override the reveal function to prevent cwd changes
+      -- on_show = function(picker)
+      --   -- Lock the cwd to the initial directory  
+      --   picker._locked_cwd = picker:cwd()
+      -- end,  
+      -- -- Prevent cwd changes on file operations
+      -- transform = function(picker, item)
+      --   if picker._locked_cwd and item.file then
+      --     -- Only show items within the locked directory
+      --     if not item.file:find("^" .. picker._locked_cwd, 1, true) then
+      --       return nil
+      --     end
+      --   end
+      --   return item
+      -- end,
       dashboard = {
         preset = {
           header = [[
